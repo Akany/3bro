779d2d6e0112b7eb10702a964d317a31fdc28d65
akany <sif.mori@gmail.com>
2015-06-14 20:09:13 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+	exec = require('child_process').exec;;
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('install', function() {
+  exec('npm install', function (error) {
+  	if (error) {
+  		console.log('npm isntall erorr');
+  	}
+
+  	gulp.src('node_modules/angular/angular.js')
+		.pipe(gulp.dest('public/javascripts/vendor/'));
+  });
+});
+
+gulp.task('start', function () {
+	exec('npm start');
+})
