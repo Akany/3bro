524a490551708a834ee06e6efbe43e1ba0f3fe63
akany <sif.mori@gmail.com>
2015-09-18 00:55:54 +0300

    moved to mongolab

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 var mongoose = require('mongoose'),
     db = mongoose.connection;
 
-mongoose.connect(process.env.MONGOHQ_URL);
+mongoose.connect(process.env.MONGOLAB_URI);
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
