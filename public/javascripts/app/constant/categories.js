5314818927967e339604bee006e529843dd0110c
akany <sif.mori@gmail.com>
2015-11-24 22:07:49 +0200

    added more categories

--- public/javascripts/app/constant/categories.js
+++ public/javascripts/app/constant/categories.js
@@ -11,5 +11,29 @@ define(function () {
 	}, {
 		name: 'Clothes',
 		value: 'clothes'
+	}, {
+		name: 'Gifts',
+		value: 'gifts'
+	}, {
+		name: 'Entertainment',
+		value: 'entertainment'
+	}, {
+		name: 'Self care',
+		value: 'selfcare'
+	}, {
+		name: 'Health',
+		value: 'health'
+	}, {
+		name: 'Traveling',
+		value: 'traveling'
+	}, {
+		name: 'Utilities',
+		value: 'utilities'
+	}, {
+		name: 'Household goods',
+		value: 'householdgoods'
+	}, {
+		name: 'Communication',
+		value: 'cominication'
 	}];
 });
